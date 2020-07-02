6ea986582420e56765ccefe1242c14f7eb1e48e7
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-07-02 12:03:13 +0200

    week 1

--- isolate/reference-vs-value/exercises/2-fill-in-blanks.js
+++ isolate/reference-vs-value/exercises/2-fill-in-blanks.js
@@ -1,15 +1,12 @@
-console.log('\n--- array challenge: 2 ---');
 // psst.  use JS Tutor & the debugger!
-
 
 const array1 = _;
 const array2 = _;
+
 const test1 = array1 !== array2;
 console.assert(test1, 'Test 1');
 
-_;
-_;
-const test2a = array2a === 'f';
-const test2b = array2b === 'f';
+const test2a = array1[0] === 'f';
+const test2b = array2[0] === 'f';
 console.assert(test2a && test2b, 'Test 2');
 
