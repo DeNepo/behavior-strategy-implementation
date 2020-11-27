ae9c190b6b5fd6b1b82cbe0482c3781ba54948cd
Bermarte <bernardo.martelli@gmail.com>
2020-11-27 20:42:49 +0100

    reversed function shoulg go backwards

--- about-testing/exercises/2-testing-functions/reverse-a-string.test.js
+++ about-testing/exercises/2-testing-functions/reverse-a-string.test.js
@@ -14,7 +14,7 @@ const reverse = (text = '') => {
   }
 
   let reversed = '';
-  for (let i = text.length - 1; i >= 0; i++) {
+  for (let i = text.length - 1; i >= 0; i--) {
     reversed += text[i];
   }
   return reversed;
