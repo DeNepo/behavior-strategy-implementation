f0210c7f5c209c3a3eeab1465ebe305b2850951c
thibault Lesuisse <thibault.lesuisse@outlook.com>
2020-11-28 17:44:36 +0100

    add two exercises

---
+++ isolate/08-objects/examples/8-pass-objects-to-functions.js
@@ -0,0 +1,16 @@
+// We start by creating a simple object
+const foods = {
+    bread: 'for dipping',
+    chili: 'a soup',
+    milk: 'drink it'
+  };
+
+  // We declare our function that accepts an object 
+  function displayFood(food){
+      for(let key of Object.keys(food)){
+          console.log(key)
+      }
+  }
+
+// We call the function with a reference
+  displayFood(foods);
