cc3a32654b045c708cb3fe2103ece256a91215d0
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-12-07 15:54:55 +0100

    remove type check

--- isolate/07-functional-array-methods/examples/3-all-together-separated.js
+++ isolate/07-functional-array-methods/examples/3-all-together-separated.js
@@ -9,14 +9,8 @@
  * @param {number[]} arr - contains to numbers to operate on
  * @returns {number} the sum of all even numbers in the array
  */
-const sumEvenNumbers = (arr) => {
-  const isANumber = (entry) => {
-    return typeof entry === 'number';
-  };
-  const allAreNumbers = arr.every(isANumber);
-  if (!allAreNumbers) { throw new TypeError('arr is not all numbers'); }
-
-  const isEven = (entry) => {
+const sumEvenNumbers = arr => {
+  const isEven = entry => {
     return entry % 2 === 0;
   };
   const sumNumbers = (sum, next) => {
@@ -38,12 +32,3 @@ const _2_arg = [-2, -3, -4, -5, -6];
 const _2_expect = -12;
 const _2_actual = sumEvenNumbers(_2_arg);
 console.assert(_2_actual === _2_expect, 'sums -1 to -6');
-
-
-try { // it should throw an error for invalid arrays
-  sumEvenNumbers([1, 2, '3', 4, 5]);
-  console.assert(false, 'is type-safe');
-} catch (err) {
-  console.assert(true, 'is type-safe');
-}
-
