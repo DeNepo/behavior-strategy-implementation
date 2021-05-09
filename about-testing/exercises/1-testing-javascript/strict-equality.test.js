ac6d22921fc4d05123bf505917e6df40b3be1370
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-05-09 11:14:42 +0200

    final line of `it` has extra dot
    as I understand, should be only one dot in the final line

--- about-testing/exercises/1-testing-javascript/strict-equality.test.js
+++ about-testing/exercises/1-testing-javascript/strict-equality.test.js
@@ -10,31 +10,31 @@ describe('strict equality compares the type and the value', () => {
     it('capital and lower case are not the same', () => {
       const actual = 'HI' === 'hi';
       const expected = false;
-      _(_).to._(_);
+      _(_).to_(_);
     });
   });
   describe('numbers', () => {
     it('extra zeros after the . do not matter', () => {
       const actual = 1 === 1.0;
       const expected = _;
-      _(_).to._(_);
+      _(_).to_(_);
     });
     it('scientific notation exists in JavaScript', () => {
       const actual = 1000 === 1e3;
       const expected = _;
-      _(_).to._(_);
+      _(_).to_(_);
     });
   });
   describe('strings and numbers', () => {
     it('some comparisons are obviously false', () => {
       const actual = 12 === 'turtle';
       const expected = _;
-      _(_).to._(_);
+      _(_).to_(_);
     });
     it('but others are more confusing', () => {
       const actual = 12 === '12';
       const expected = _;
-      _(_).to._(_);
+      _(_).to_(_);
     });
   });
 });
